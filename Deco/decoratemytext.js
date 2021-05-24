d664577e47b72ca8144e93865eff1854bdd8da27
Quest <b.quest@hotmai.com>
2021-05-24 13:30:31 -0500

    added bling checkbox

--- Deco/decoratemytext.js
+++ Deco/decoratemytext.js
@@ -5,4 +5,19 @@ function display() {
 
 function popAlert(){
     alert('You changed me: ' + document.getElementById("blingCheckbox").name);
+    let textArea = document.getElementById('summary');
+    let fontWeight = textArea.style.fontWeight;
+    let color = textArea.style.color;
+    let textDecoration = textArea.style.textDecoration;
+    let blingCheckbox = document.getElementById('blingCheckbox');
+    if(blingCheckbox.checked) {
+        textArea.style.fontWeight = 'bold';
+        textArea.style.color = 'green';
+        textArea.style.textDecoration = 'underline';
+    } else {
+        textArea.style.fontWeight = "";
+        textArea.style.color = "";
+        textArea.style.textDecoration = "";
+    }
+
 }
