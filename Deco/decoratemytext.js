956bbb7919a02e95a924ebff9e0c50ba410b2bd1
Quest <b.quest@hotmai.com>
2021-05-24 20:40:54 -0500

    modified display

--- Deco/decoratemytext.js
+++ Deco/decoratemytext.js
@@ -1,37 +1,25 @@
-window.onload = function() {
+window.onload = function () {
     document.getElementById('summary').onclick = display;
 };
 
 function display() {
-    'Use Strict'
-    //alert("Hello, world");
-    //document.getElementById('summary').style.fontSize = "24pt";
-    // let textArea = document.getElementById('summary');
-    // let size = parseInt(textArea.style.fontSize);
-    // textArea.style.fontSize = size + 2 + "px";
-    var elem = document.getElementById('summary');
-    console.log(window.getComputedStyle(elem, null).getPropertyValue('font-size'));
-    let size = document.getElementById('summary').style.fontSize;
-    console.log(size);
+    alert("Hello, world");
 
-
-    // var el = document.getElementById('foo');
-    // var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
-    // var fontSize = parseFloat(style);
-    // // now you have a proper float for the font size (yes, it can be a float, not just an integer)
-    // el.style.fontSize = (fontSize + 1) + 'px';
+    let textArea = document.getElementById('summary');
+    let size = parseInt(window.getComputedStyle(textArea, null).getPropertyValue('font-size'));
+    textArea.style.fontSize = size + 2 + "px";
 }
 
 
 
-function popAlert(){
+function popAlert() {
     alert('You changed me: ' + document.getElementById("blingCheckbox").name);
     let textArea = document.getElementById('summary');
     let fontWeight = textArea.style.fontWeight;
     let color = textArea.style.color;
     let textDecoration = textArea.style.textDecoration;
     let blingCheckbox = document.getElementById('blingCheckbox');
-    if(blingCheckbox.checked) {
+    if (blingCheckbox.checked) {
         textArea.style.fontWeight = 'bold';
         textArea.style.color = 'green';
         textArea.style.textDecoration = 'underline';
