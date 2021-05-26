9ea7adb5cf88322f6234f1a0c4fe5f2f558d5d80
Quest <b.quest@hotmai.com>
2021-05-26 01:14:33 -0500

    added change to animation in text area

--- ASCII/ascii.js
+++ ASCII/ascii.js
@@ -2,7 +2,7 @@
 
 window.onload = function() {
     document.getElementById('fontSizes').onchange = setTextAreaFontSize;
-  
+    document.getElementById('animations').onchange = setTextAreaContent;
 }
 
 let start = document.getElementById('btnstart').onclick = function() {
@@ -14,4 +14,9 @@ function setTextAreaFontSize() {
     let textArea = document.getElementById('animationview');
     let size = parseInt(document.getElementById('fontSizes').value);
     textArea.style.fontSize = size + 'pt';
+}
+
+function setTextAreaContent() {
+    let textArea = document.getElementById('animationview');
+    textArea.innerHTML = ANIMATIONS[document.getElementById('animations').value];
 }
