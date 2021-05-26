b92fe157d57e07b9a186e519c743ea520111fdcb
Quest <b.quest@hotmai.com>
2021-05-26 08:41:51 -0500

    added rudy content

--- ASCII/ascii.js
+++ ASCII/ascii.js
@@ -1,13 +1,24 @@
 "use strict";
-
+var animation;
 window.onload = function() {
-    document.getElementById('fontSizes').onchange = setTextAreaFontSize;
-    document.getElementById('animations').onchange = setTextAreaContent;
-}
+    var x = 1;
+    function h() {
+        var k = 2;
+        var summ = function () {
+            var z = 3;
+            console.log(x + k + z);
+        };
+        k = 10;
+        //z = 12;
+        return summ;
+    }
+    var g = h();
+    g();
 
-let start = document.getElementById('btnstart').onclick = function() {
-    let textArea = document.getElementById('animationview');
-    textArea.innerHTML = ANIMATIONS["Exercise"];
+    // document.getElementById('btnstart').onclick = p;
+    // document.getElementById('fontSizes').onchange = setTextAreaFontSize;
+    // // document.getElementById('animations').onchange = setTextAreaContent;
+    // document.getElementById('btnstart').onclick = startAnimation;
 }
 
 function setTextAreaFontSize() {
@@ -16,7 +27,51 @@ function setTextAreaFontSize() {
     textArea.style.fontSize = size + 'pt';
 }
 
-function setTextAreaContent() {
-    let textArea = document.getElementById('animationview');
-    textArea.innerHTML = ANIMATIONS[document.getElementById('animations').value];
+let textAreaContent = function () {
+    // let dropdown = document.getElementById('animations');
+    return ANIMATIONS[document.getElementById('animations').value];
 }
+
+// function startAnimation() {
+//     // if()
+//     // let selectedAnimation = document.getElementById('animations').value;
+//     // let selectedAnimation = do
+//     // let frame = ANIMATIONS[selectedAnimation].split("=====\n");
+//     // alert(frame.length);
+
+// }
+
+function animateText(arr) {
+    arr.forEach(frame => {
+        document.getElementById('animationview').innerHTML += frame;
+    });
+    
+}
+
+function f(){
+    var a = 1, b = 20, c;
+    console.log(a + " " + b + " " + c);
+
+    function g() {
+        var b = 300, c = 4000;
+        console.log(a + " " + b + " " + c);
+        a = a + b + c;
+        console.log(a + " " + b + " " + c);
+    }
+    console.log(a + " " + b + " " + c);
+    g();
+    console.log(a + " " + b + " " + c);
+}
+function p() {
+    for(var i = 0; i < 10; i++) {
+        console.log(i);
+    }
+    console.log("=====");
+    console.log(i);
+    if(i > 5) {
+        var j = 3;
+    }
+    console.log(j);
+}
+
+
