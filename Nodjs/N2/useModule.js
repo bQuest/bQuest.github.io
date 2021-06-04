b140c411e263ef33f1b4d6651babd5af26f77cc2
Quest <b.quest@hotmai.com>
2021-06-03 23:16:36 -0500

    added n3 content

--- Nodjs/N2/useModule.js
+++ Nodjs/N2/useModule.js
@@ -1,5 +1,8 @@
 var http = require('http');
+var dt = require('./myModule');
+
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World!');
+    res.write("The date and time arecurrently: " + dt.myDate());
+    res.end();
 }).listen(8080);
