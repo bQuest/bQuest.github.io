7e2db75cfc29a33933a025752b97d550f6d019d2
Quest <b.quest@hotmai.com>
2021-06-05 07:41:42 -0500

    added simple calc link

--- SimpleCalc/calmodule.js
+++ SimpleCalc/calmodule.js
@@ -15,22 +15,6 @@ exports.add = function (req, res, vals) {
     return res.end();
 };
 
-exports.subtract = function (req, res, vals) {
-    var diff = parseInt(vals.first) - parseInt(vals.second);
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write("<!DOCTYPE html>");
-    res.write("<html>");
-    res.write("<head><meta charset=\"utf-8\"/>");
-    res.write("<title>Calculator Web Site</title>");
-    res.write("</head>");
-    res.write("<body>");
-    res.write("<p>The result is: ");
-    res.write(String(diff));
-    res.write("</p>");
-    res.write("</body>");
-    res.write("</html>");
-    return res.end();
-};
 
 exports.multiply = function (req, res, vals) {
     var product = parseInt(vals.first) * parseInt(vals.second);
